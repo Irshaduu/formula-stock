33e3294a1ecbc1ac8f60933e0a00e9c5af428911
Mohammed Irshad <irshaduu01@gmail.com>
2026-02-03 10:12:54 +0530

    chore: Configure settings for deployment (ALLOWED_HOSTS, STATIC_ROOT, gitignore DB)

--- formula_d_store/settings.py
+++ formula_d_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nz8!#c#dt4%3y#^^&=#+a0*_n=sr_)faa*7_#l+mh6*tf1c^wl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
